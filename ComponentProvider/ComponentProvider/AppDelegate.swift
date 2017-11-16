b18c1e27bc9f4b72f55ff7bbe1b486f6abb3040e
Roy Hsu <roy.hsu@tinyworld.cc>
2017-11-16 21:54:48 +0800

    add xcode project

---
+++ ComponentProvider/ComponentProvider/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  ComponentProvider
+//
+//  Created by Roy Hsu on 16/11/2017.
+//  Copyright © 2017 TinyWorld. All rights reserved.
+//
+
+// MARK: - AppDelegate
+
+import UIKit
+
+@UIApplicationMain
+public final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Property
+    
+    public final var window: UIWindow?
+
+    // MARK: UIApplicationDelegate
+    
+    public final func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    )
+    -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = UIViewController()
+        
+        viewController.view.backgroundColor = .white
+        
+        window.rootViewController = viewController
+        
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+        
+    }
+
+}
